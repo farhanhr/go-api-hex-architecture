0c17f7ff2bd96eca50a0976e2bfb0cd2f4af9f60
Farhan Husyen Ramadhan <122083217+farhanhr@users.noreply.github.com>
2025-05-14 05:47:44 +0700

    config db

---
+++ config/database.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/rs/zerolog/log"
+	"gorm.io/gorm"
+	"gorm.io/driver/postgres"
+)
+
+type Postgres struct {
+	DB *gorm.DB
+}
+
+func (cfg Config) ConnectionPostgres() (*Postgres, error) {
+	dbConnString := fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
+		cfg.Psql.User,
+		cfg.Psql.Password,
+		cfg.Psql.Host,
+		cfg.Psql.Port,
+		cfg.Psql.DBName)
+
+	db, err := gorm.Open(postgres.Open(dbConnString), &gorm.Config{})
+	if err != nil {
+		log.Error().Err(err).Msg("[ConnectionPostgres-1] Failed connect to database" + cfg.Psql.Host)
+		return nil, err
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Error().Err(err).Msg("[ConnectionPostgres-2] Failed get database connection" + cfg.Psql.Host)
+		return nil, err
+	}
+
+	sqlDB.SetMaxOpenConns(cfg.Psql.DBMaxOpen)
+	sqlDB.SetMaxIdleConns(cfg.Psql.DBMaxIdle)
+
+	return &Postgres{DB: db}, nil
+}
